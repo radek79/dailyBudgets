562f7c6c3942ef15b24ca7663ac3d8c1427015a8
Rod Patulski <rpatulski@wsoutput.onpeak.com>
2016-08-06 21:23:37 -0700

    displaying some hardcoded data to a page

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,10 +1,20 @@
 import {Page} from 'ionic-angular';
 
+interface Budget {
+	title : string;
+	available : number;
+	total : number;
+}	
+
+
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor() {
 
-  }
+	monthlyBudget: Budget;
+
+	constructor() {
+		this.monthlyBudget = {title: 'my monthly budget', available: 150, total: 750};
+	}
 }
